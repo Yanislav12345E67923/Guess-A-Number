4a5d4d805baa200b20263970ab6b1090139a5574
Yanislav12345E67923 <yanislav1234567@gmail.com>
2023-05-22 23:23:38 +0300

    The game

---
+++ Guess a Number - Additional Project/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Guess_a_Number___Additional_Project;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rn = new Random();
+        int chosenNumber = rn.Next(1, 101);
+
+        string input = null;
+        bool isInt = false;
+        while (true)
+        {
+            Console.Write("Guess a number (1-100): ");
+            input = Console.ReadLine();
+
+            isInt = int.TryParse(input, out int a);
+
+            if (!isInt || int.Parse(input) > 100 || int.Parse(input) < 1)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            else
+            {
+                if (int.Parse(input) > chosenNumber)
+                {
+                    Console.WriteLine("Too High");
+                }
+                else if (int.Parse(input) < chosenNumber)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+        }
+    }
+}
